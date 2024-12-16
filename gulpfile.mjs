8041348ca9b04da4032f1cddc0d0819f40d42114
MelnikovS5 <semenmelnikov87@gmail.com>
2024-12-16 20:49:58 +0700

    winter comit

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -92,22 +92,25 @@ function jsCopy() {
 function copy() {
   return gulp
     .src(resources.static, {
-      base: "src"
+      base: "src",
+      encoding: false
     })
     .pipe(gulp.dest("dist/"));
 }
+
 function images() {
   return gulp
-    .src(resources.images)
+    .src(resources.images, { encoding: false })
     .pipe(
       imagemin([
         imagemin_gifsicle({ interlaced: true }),
         imagemin_mozjpeg({ quality: 100, progressive: true }),
-        imagemin_optipng({ optimizationLevel: 3 })
+        imagemin_optipng({ optimizationLevel: 5 })
       ])
     )
-    .pipe(gulp.dest("dist/assets/images"));
+    .pipe(gulp.dest('dist/assets/images'));
 }
+
 function svgSprite() {
   return gulp
     .src(resources.svgSprite)
